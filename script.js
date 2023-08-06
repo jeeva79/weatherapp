ac4197ac69887ac843592509988a1f888b398ef9
Jeeva <125794481+jeeva79@users.noreply.github.com>
2023-08-06 21:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    const api_key = "1b341d10a0af3e440b5857c3b0b3298b";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
